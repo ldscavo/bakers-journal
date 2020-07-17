066dd7034f485f92d9481502b3aaa7f712dcbd63
Logan Scavo <logan@scavo.dev>
2020-07-16 22:31:44 -0400

    Added some tests around the baker's percentages

---
+++ test/baking_diary/recipe_test.clj
@@ -0,0 +1,26 @@
+(ns baking-diary.recipe-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [baking-diary.recipe :as recipe]))
+
+(deftest test-recipe
+  (let [dough {:flour 500}]
+    
+    (testing "Hydration is calculated correctly"
+      (let [dough-with-water (recipe/water 75 dough)]
+        (is (= (:hydration dough-with-water) 75))
+        (is (= (:water dough-with-water) 375))))
+    
+    (testing "Levain is calculated correctly"
+      (let [dough-with-levain (recipe/levain dough)]
+        (is (= (:levain dough-with-levain) 100))))
+    
+    (testing "Salt is calculated correctly"
+      (let [dough-with-salt (recipe/salt dough)]
+        (is (= (:salt dough-with-salt) 10)))))
+  
+  (testing "Create recipe function results in all four ingredients"
+    (let [recipe (recipe/create-recipe 500 75)]
+      (is (contains? recipe :flour))
+      (is (contains? recipe :water))
+      (is (contains? recipe :levain))
+      (is (contains? recipe :salt)))))
